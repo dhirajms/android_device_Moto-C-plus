9d02ac50d45047c4fa5b56ece9dbd2da9e8a4617
DhirajSurvase <dms25121997@gmail.com>
2018-01-31 01:21:37 +0530

    auto apply patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,35 @@
 for var in eng user userdebug; do
   add_lunch_combo lineage_panelli-$var
 done
+
+# Patches
+cd frameworks/av
+git reset --hard && git clean -f -d
+cd ../..
+cd frameworks/base
+git reset --hard && git clean -f -d
+cd ../..
+cd frameworks/native
+git reset --hard && git clean -f -d
+cd ../..
+cd system/netd
+git reset --hard && git clean -f -d
+cd ../..
+cd system/core
+git reset --hard && git clean -f -d
+cd ../..
+cd frameworks/av
+patch -p1 < ../../device/moto/panelli/patches/frameworks_av.diff
+cd ../..
+cd frameworks/base
+patch -p1 < ../../device/moto/panelli/patches/frameworks_base.diff
+cd ../..
+cd system/netd
+patch -p1 < ../../device/moto/panelli/patches/system_netd.diff
+cd ../..
+cd system/core
+patch -p1 < ../../device/moto/panelli/patches/system_core.diff
+cd ../..
+cd frameworks/native
+patch -p1 < ../../device/moto/panelli/patches/frameworks_native.diff
+cd ../..
