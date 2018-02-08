3178b93ec7d11c401ddda140e242baedd87a4274
DhirajSurvase <dms25121997@gmail.com>
2018-02-08 23:09:23 +0530

    panelli: update trees

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),namath)
+ifeq ($(TARGET_DEVICE),panelli)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,3 @@ include $(CLEAR_VARS)
 
 #ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
-# include the non-open-source counterpart to this file
--include vendor/moto/panelli/AndroidBoardVendor.mk
