272b21ba3944c4657dd02b5a58dc1c11bf67fca1
DhirajSurvase <dms25121997@gmail.com>
2018-02-09 09:59:52 +0000

    use proper scale and bootanimation

--- lineage.mk
+++ lineage.mk
@@ -9,6 +9,14 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/moto/panelli/panelli.mk)
 
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+DEVICE_RESOLUTION := 720x1280
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := panelli
 PRODUCT_NAME := lineage_panelli
