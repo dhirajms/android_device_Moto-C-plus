bfe33dfb74aef55fbea8c2dfc6f1c7a70365810a
DhirajSurvase <dms25121997@gmail.com>
2018-02-13 06:05:19 +0000

    panelli: update patches

---
+++ patches/install.sh
@@ -0,0 +1,17 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="frameworks/av frameworks/base frameworks/native system/netd system/core"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git apply $rootdirectory/device/moto/panelli/patches/$dir/*.patch
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,36 +3,7 @@ for var in eng user userdebug; do
 done
 
 # Patches
-cd frameworks/av
-git reset --hard && git clean -f -d
-cd ../..
-cd frameworks/base
-git reset --hard && git clean -f -d
-cd ../..
-cd frameworks/native
-git reset --hard && git clean -f -d
-cd ../..
-cd system/netd
-git reset --hard && git clean -f -d
-cd ../..
-cd system/core
-git reset --hard && git clean -f -d
-cd ../..
-cd frameworks/av
-patch -p1 < ../../device/moto/panelli/patches/frameworks_av.diff
-cd ../..
-cd frameworks/base
-patch -p1 < ../../device/moto/panelli/patches/frameworks_base.diff
-cd ../..
-cd system/netd
-patch -p1 < ../../device/moto/panelli/patches/system_netd.diff
-cd ../..
-cd system/core
-patch -p1 < ../../device/moto/panelli/patches/system_core.diff
-cd ../..
-cd frameworks/native
-patch -p1 < ../../device/moto/panelli/patches/frameworks_native.diff
-cd ../..
-cd external/wpa_supplicant_8
-patch -p1 < ../../device/moto/panelli/patches/external_nvram.patch
-cd ../..
+bash device/moto/panelli/patches/uninstall.sh
+
+bash device/moto/panelli/patches/install.sh
+
