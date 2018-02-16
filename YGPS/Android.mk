687f3637eec5d467f1f93f54c7a82a460fa5b033
DhirajSurvase <dms25121997@gmail.com>
2018-02-16 18:30:11 +0530

    panelli: add YGPS

---
+++ YGPS/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+
+LOCAL_OVERRIDES_PACKAGES := CMUpdater CMBugReport ResurrectionOTA ResurrectionStats MagiskManager Camera2
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := YGPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PROGUARD_ENABLED := disabled
+
+include $(BUILD_PACKAGE)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
