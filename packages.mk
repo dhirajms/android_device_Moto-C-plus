094d6b548e13898c45a79783967a192270cc5db7
DhirajMS <dms25121997@gmail.com>
2018-03-10 05:09:02 +0530

    panelli: [cleanup(2/2)]

---
+++ packages.mk
@@ -0,0 +1,56 @@
+# Root
+PRODUCT_PACKAGES += \
+    fstab.mt6735 \
+    init.mt6735.rc \
+    init.mt6735.conn.rc \
+    init.mt6735.modem.rc \
+    init.mt6735.power.rc \
+    init.mt6735.usb.rc \
+    ueventd.mt6735.rc
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libaudio-resampler \
+    libtinyalsa \
+    libtinycompress \
+    libtinymix \
+    libtinyxml
+    
+# Mediatek platform
+PRODUCT_PACKAGES += \
+    libmtk_symbols
+    
+# Power
+PRODUCT_PACKAGES += \
+    power.default \
+    power.mt6737m
+
+# Wifi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    lib_driver_cmd_mt66xx
+    
+PRODUCT_PACKAGES += \    
+    librs_jni \
+    com.android.future.usb.accessory \
+    charger \
+    charger_res_images \
+    libnl_2 \
+    libion \
+    Snap
+    
+ # FM
+PRODUCT_PACKAGES += \
+    libfmjni \   
+    FMRadio 
+    
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0
