22e3b2482073b8f33367b0929b954429fc20122f
DhirajSurvase <dms25121997@gmail.com>
2018-03-25 10:31:50 +0000

    Update bt config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -2,7 +2,8 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Moto C Plus"
+// test
+//#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLE_VND_INCLUDED   TRUE
-
 #endif
