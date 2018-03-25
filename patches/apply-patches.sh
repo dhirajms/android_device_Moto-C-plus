2d8a981f14a570dc4e28930fc2d22f0129ee2fe2
DhirajSurvase <dms25121997@gmail.com>
2018-03-25 10:20:23 +0000

    patches from darklord's repo

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 cd ../../../..
-cd frameworks/av
-git apply -v ../../device/moto/panelli/patches/0001-frameworks_av.patch
-cd ../..
-cd frameworks/base
-git apply -v ../../device/moto/panelli/patches/0002-frameworks_base.patch
-cd ../..
-cd frameworks/native
-git apply -v ../../device/moto/panelli/patches/0003-frameworks_native.patch
-cd ../..
-cd system/netd
-git apply -v ../../device/moto/panelli/patches/0004-system_netd.patch
-cd ../..
 cd system/core
-git apply -v ../../device/moto/panelli/patches/0005-system_core.patch
+git apply -v ../../device/moto/panelli/patches/0001-system_core.patch
+cd ../..
+cd hardware/interfaces
+git apply -v ../../device/moto/panelli/patches/0002-hardware_interfaces.patch
 cd ../..
