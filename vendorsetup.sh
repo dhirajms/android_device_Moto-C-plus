3d6b6b1b485050b4266d1148c3ffbb0312e8e8d0
DhirajSurvase <dms25121997@gmail.com>
2018-04-14 22:46:43 +0000

    apply patch mannuly nd die

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,3 @@ for var in eng user userdebug; do
   add_lunch_combo lineage_panelli-$var
 done
 
-# Patches
-bash device/moto/panelli/patches/uninstall.sh
-
-bash device/moto/panelli/patches/install.sh
-
