a09ed564723e251ab34a2a4942f3c09fc4e266ec
DhirajSurvase <dms25121997@gmail.com>
2018-05-24 16:29:42 +0000

    A158: clone devicesettings packege
    m bit lazy

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ for var in eng user userdebug; do
   add_lunch_combo lineage_panelli-$var
 done
 
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b cm-14.1 packages/resources/devicesettings
